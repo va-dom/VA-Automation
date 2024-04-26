18164f3676b1caa67da3fa6b427ead9d62809869
Vole Aggabao <volejunzen.aggabao@via-appia.ph>
2024-04-26 13:22:11 +0800

    Buttons demo

---
+++ va_lsg/Vole/reusable_components_vole/lib/src/components/elevated_button_demo.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:va_reusable_components/va_reusable_components.dart';
+
+class ElevatedButtonDemo extends StatelessWidget {
+  const ElevatedButtonDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Elevated Button",
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.bold),
+        ),
+        const Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ButtonElevated(
+              onPressed: () {},
+              label: "Primary",
+              leadingIcon: null,
+              trailingIcon: null,
+              isLoading: false,
+              isDisabled: false,
+            ),
+            const SizedBox(width: 8),
+            ButtonElevated(
+              onPressed: () {},
+              label: "Loading",
+              leadingIcon: null,
+              trailingIcon: null,
+              isLoading: true,
+              isDisabled: false,
+            ),
+            const SizedBox(width: 8),
+            ButtonElevated(
+              onPressed: () {},
+              label: "Disabled",
+              leadingIcon: null,
+              trailingIcon: null,
+              isLoading: false,
+              isDisabled: true,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
---
+++ va_lsg/Vole/reusable_components_vole/lib/src/components/primary_button_demo.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:reusable_demo/src/buttontest.dart';
+// import 'package:va_reusable_components/va_reusable_components.dart';
+
+class PrimaryButtonDemo extends StatelessWidget {
+  const PrimaryButtonDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Primary Button",
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Montserrat',
+              ),
+        ),
+        Text(
+          "Primary Button",
+          style: TextStyle(
+            // fontWeight: FontWeight.bold,
+            fontFamily: 'Montserrat',
+          ),
+        ),
+        const Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ButtonPrimary(
+              onPressed: () {},
+              label: "Primary",
+              leadingIcon: null,
+              trailingIcon: null,
+              isLoading: false,
+              isDisabled: false,
+            ),
+            const SizedBox(width: 8),
+            ButtonPrimary(
+              onPressed: () {},
+              label: "Loading",
+              leadingIcon: null,
+              trailingIcon: null,
+              isLoading: true,
+              isDisabled: false,
+            ),
+            const SizedBox(width: 8),
+            ButtonPrimary(
+              onPressed: () {},
+              label: "Disabled",
+              leadingIcon: null,
+              trailingIcon: null,
+              isLoading: false,
+              isDisabled: true,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
